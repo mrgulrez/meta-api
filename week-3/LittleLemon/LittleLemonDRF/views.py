34713b1a25be8bbe9e495acc3f3c73947d80336e
Gulrez Alam <egulrezalam@gmail.com>
2023-07-18 19:27:46 +0530

    updated

---
+++ week-3/LittleLemon/LittleLemonDRF/views.py
@@ -0,0 +1,17 @@
+from rest_framework import generics
+from .serializers import MenuItemSerializer, CategorySerializer
+from .models import MenuItem, Category
+
+
+
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    ordering_fields = ['price', 'inventory']
+    filterset_fields = ['price', 'inventory']
+    search_fields = ['category']
