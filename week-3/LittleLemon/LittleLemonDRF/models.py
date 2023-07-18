678032dbd762b26f7db02d73f912fd46952e7eeb
Gulrez Alam <egulrezalam@gmail.com>
2023-07-19 02:10:31 +0530

    upadted

--- week-3/LittleLemon/LittleLemonDRF/models.py
+++ week-3/LittleLemon/LittleLemonDRF/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class Rating(models.Model):
-    menuitem_id = models.SmallIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem_id =  models.SmallIntegerField()
     rating = models.SmallIntegerField()
-    category = models.ForeignKey(User, on_delete=models.CASCADE)
--- week-3/LittleLemon/LittleLemonDRF/serializers.py
+++ week-3/LittleLemon/LittleLemonDRF/serializers.py
@@ -1,22 +1,26 @@
-
 from rest_framework import serializers
 from .models import Rating
 from rest_framework.validators import UniqueTogetherValidator
 from django.contrib.auth.models import User
 
-
 class RatingSerializer (serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(
-    queryset=User.objects.all(),
-    default=serializers.CurrentUserDefault()
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+)
+
+class Meta:
+    model = Rating
+    fields = ['user', 'menuitem_id', 'rating']
+
+validators = [
+    UniqueTogetherValidator(
+        queryset=Rating.objects.all(),
+        fields=['user', 'menuitem_id']
     )
-    class Meta:
-        model = Rating
-        fields = {'user', 'menuitem_id', 'rating'}
-        validators = UniqueTogetherValidator(queryset=Rating.objects.all(), fields={'user', 'menuitem_id', 'rating'})
-        extra_kwargs = {
-            'rating': {
-                'max_value':5,
-                'min_value':0
-            }
-        }
+]
+
+extra_kwargs = {
+    'rating': {'min_value': 0, 'max_value':5},
+}
+
